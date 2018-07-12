dbf9c097af27851b9f810826193cb56fcd93a5a8
Avinash R Periyavaram <39273273+avinashperiyavaram@users.noreply.github.com>
2018-07-11 23:36:59 -0400

    Update matrix cache.R

--- matrix cache.R
+++ matrix cache.R
@@ -1,17 +1,18 @@
+# makeCacheMatrix function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x=matrix()){
-                                        inverse1 <- NULL
+                                        inverse1 <- NULL 
                                         setA <- function(y) {
                                                                 x <<- y
                                                                 inverse1 <- NULL
-                                        }
-                                        getA <- function() x
+                                        } #Creation of a set 
+                                        getA <- function() x #Creation of return object
                                         setAinverse1 <- function(inverse) inverse1 <<- inverse
                                         getAinverse1 <- function() inverse1
                                         list( setA = setA, getA = getA, setAinverse1 = setAinverse1, getAinverse1 = getAinverse1)
 }
-
+#cacheSolve function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-                                inverse1 <- x$getAinverse1()
+                                inverse1 <- x$getAinverse1() #Requesting to return a matrix that is inverse of "x"
                                 if(!is.null(inverse1)){
                                         print("Cache on the way")
                                         return(inverse1)
@@ -21,3 +22,4 @@ cacheSolve <- function(x, ...) {
                                 x$setAinverse1(inverse1)
                                 inverse1
 }
+#End 
